52ba6571916f75aaff09788bd170f0fd5e522f3d
Mohd Rejoan <wearecrx@gmail.com>
2023-02-04 16:58:15 +0600

    track ball updated

--- js/script.js
+++ js/script.js
@@ -1,144 +1,3 @@
-var elms = [
-    "track",
-    "title",
-    "subtitle",
-    "timer",
-    "duration",
-    "playBtn",
-    "pauseBtn",
-    "progress",
-    "thumb",
-    "c-thumb",
-];
-elms.forEach(function (elm) {
-    window[elm] = document.getElementById(elm);
-});
-
-var Player = function (playlist) {
-    this.playlist = playlist;
-    this.index = 0;
-};
-Player.prototype = {
-    /**
-     * Play a song in the playlist.
-     * @param  {Number} index Index of the song in the playlist (leave empty to play the first or current).
-     */
-    play: function (index) {
-        var self = this;
-        var sound;
-
-        index = typeof index === "number" ? index : self.index;
-        var data = self.playlist[index];
-
-        // If we already loaded this track, use the current one.
-        // Otherwise, setup and load a new Howl.
-        if (data.howl) {
-            sound = data.howl;
-        } else {
-            sound = data.howl = new Howl({
-                src: [
-                    "https://res.cloudinary.com/n3pu/video/upload/v1571849541/80s_vibe.mp3",
-                ],
-                html5: true, // Force to HTML5 so that the audio can stream in (best for large files).
-                onplay: function () {
-                    // Display the duration.
-                    duration.innerHTML = self.formatTime(
-                        Math.round(sound.duration())
-                    );
-                    // track.classList.add("up");
-                    // Start upating the progress of the track.
-                    requestAnimationFrame(self.step.bind(self));
-                },
-            });
-        }
-
-        // Begin playing the sound.
-        sound.play();
-
-        // Show the pause button.
-        if (sound.state() === "loaded") {
-            playBtn.style.display = "none";
-            pauseBtn.style.display = "block";
-        } else {
-            playBtn.style.display = "none";
-            pauseBtn.style.display = "block";
-            // track.classList.remove("up");
-        }
-
-        // Keep track of the index we are currently playing.
-        self.index = index;
-    },
-
-    /**
-     * Pause the currently playing track.
-     */
-    pause: function () {
-        var self = this;
-
-        // Get the Howl we want to manipulate.
-        var sound = self.playlist[self.index].howl;
-
-        // Puase the sound.
-        sound.pause();
-
-        // Show the play button.
-        playBtn.style.display = "block";
-        pauseBtn.style.display = "none";
-    },
-
-    /**
-     * The step called within requestAnimationFrame to update the playback position.
-     */
-    step: function () {
-        var self = this;
-        // Get the Howl we want to manipulate.
-        var sound = self.playlist[self.index].howl;
-        // Determine our current seek position.
-        // get seek value
-        var seek = sound.seek() || 0;
-        progress.setAttribute(
-            "value",
-            (sound.seek() / sound.duration()) * 100 || 0
-        );
-        // If the sound is still playing, continue stepping.
-        if (sound.playing()) {
-            requestAnimationFrame(self.step.bind(self));
-        }
-    },
-
-    /**
-     * Format the time from seconds to M:SS.
-     * @param  {Number} secs Seconds to format.
-     * @return {String}      Formatted time.
-     */
-    formatTime: function (secs) {
-        var minutes = Math.floor(secs / 60) || 0;
-        var seconds = secs - minutes * 60 || 0;
-
-        return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-    },
-};
-
-// Setup our new audio player class and pass it the playlist.
-var player = new Player([
-    {
-        title: "Rave Digger",
-        subtitle: "80s vibe",
-        file: "rave_digger",
-        howl: null,
-    },
-]);
-
-// Bind our player controls.
-playBtn.addEventListener("click", function () {
-    player.play();
-});
-pauseBtn.addEventListener("click", function () {
-    player.pause();
-});
-
-// -----------------------------------------------------------------------------------------------------
-
 var timer = 0;
 
 var timerFunction;
@@ -155,9 +14,10 @@ audio1.once("load", function () {
         return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
     }
     const durationElement = document.querySelector("#duration1");
-    durationElement.innerHTML = formatTime(Math.round(audio1.duration()))
+    durationElement.innerHTML = formatTime(Math.round(audio1.duration()));
     // update the progress bar
     const progress1 = document.querySelector("#progress1");
+    progress1.setAttribute("max", audio1._duration);
     // play
     const play1 = document.querySelector("#playBtn1");
     const pauseBtn1 = document.querySelector("#pauseBtn1");
@@ -169,11 +29,27 @@ audio1.once("load", function () {
         // start timer function
         timerFunction = setInterval(() => {
             // update timer
+            console.log(timer)
 
             progress1.setAttribute("value", timer);
             timer++;
+            console.log(audio1.seek() > timer)
+            // if (Math.round(audio1.seek()) <= timer) {
+            //   timer = 1;
+            //   clearInterval(timerFunction);
+            // }
         }, 1000);
     });
+
+    // audio1.on("seek", () => {
+    //   clearInterval(timerFunction);
+    //     timerFunction = setInterval(() => {
+    //         // update timer
+
+    //         progress1.setAttribute("value", timer);
+    //         timer++;
+    //     }, 1000);
+    // });
 
     // pause
     const pause1 = document.querySelector("#pauseBtn1");
@@ -190,9 +66,8 @@ audio1.once("load", function () {
 
     progress1.addEventListener("change", (e) => {
         timer = e.target.value;
-        // audio1.seek(timer * audio1.duration());
-        // console.log(timer * audio1.duration());
-        // console.log(audio1.duration())
+        progress1.setAttribute("value", e.target.value);
+        audio1.seek(e.target.value);
     });
 
     // change speed of audio
@@ -212,9 +87,6 @@ audio1.once("load", function () {
         }
         // self.formatTime(Math.round(sound.duration()));
         console.log(formatTime(Math.round(audio1.duration())));
-        // console.log(audio1._muted);
-        // audio1.mute(true);
-        // audio1._muted ? audio1.mute(false) : audio1.mute(true);
         if (audio1._muted) {
             audio1.mute(false);
             sound.classList.remove("down");
@@ -226,5 +98,7 @@ audio1.once("load", function () {
         }
     });
 
-    console.log(audio1);
+    audio1.on('end', function () {
+      console.log('song complete')
+    })
 });
