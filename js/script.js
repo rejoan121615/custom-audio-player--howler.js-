3cc2c4e45deba6ded6c5f0dcd9b67f54da2b7f3a
Mohd Rejoan <wearecrx@gmail.com>
2023-02-04 15:07:15 +0600

    speed control added

--- js/script.js
+++ js/script.js
@@ -137,39 +137,66 @@ pauseBtn.addEventListener("click", function () {
     player.pause();
 });
 
-// ------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------
+
+
 var timer = 0;
 
 var timerFunction;
 
 var audio1 = new Howl({
-    src: "https://res.cloudinary.com/n3pu/video/upload/v1571849541/80s_vibe.mp3",
+    src: "../assets/audio.mp3",
 });
 
-// play
-const play1 = document.querySelector("#playBtn1");
-const pauseBtn1 = document.querySelector("#pauseBtn1");
 
-play1.addEventListener("click", () => {
-    audio1.play();
-    play1.style.display = "none";
-    pauseBtn1.style.display = "block";
-    // start timer function
-    timerFunction = setInterval(() => {
-        // update timer
-        const progress1 = document.querySelector("#progress1");
-        progress1.setAttribute("value", timer);
-        timer++;
-    }, 1000);
-});
+audio1.once("load", function () {
+  // update the progress bar 
+  const progress1 = document.querySelector("#progress1");
+    // play
+    const play1 = document.querySelector("#playBtn1");
+    const pauseBtn1 = document.querySelector("#pauseBtn1");
 
-// pause
-const pause1 = document.querySelector("#pauseBtn1");
+    play1.addEventListener("click", () => {
+        audio1.play();
+        play1.style.display = "none";
+        pauseBtn1.style.display = "block";
+        // start timer function
+        timerFunction = setInterval(() => {
+            // update timer
+            
+            progress1.setAttribute("value", timer);
+            timer++;
+        }, 1000);
+    });
 
-pauseBtn1.addEventListener("click", () => {
-    audio1.pause();
-    pauseBtn1.style.display = "none";
-    play1.style.display = "block";
-    // clear interval
-    clearInterval(timerFunction);
+    // pause
+    const pause1 = document.querySelector("#pauseBtn1");
+
+    pauseBtn1.addEventListener("click", () => {
+        audio1.pause();
+        pauseBtn1.style.display = "none";
+        play1.style.display = "block";
+        // clear interval
+        clearInterval(timerFunction);
+    });
+
+    // change progress bar on interaction
+
+    progress1.addEventListener("change", (e) => {
+        timer = e.target.value;
+        // audio1.seek(timer * audio1.duration());
+        // console.log(timer * audio1.duration());
+        // console.log(audio1.duration())
+    });
+
+    // change speed of audio 
+    const speed = document.querySelector("#speed1");
+    speed.addEventListener('change', (e) => {
+      audio1.rate(e.target.value)
+    })
+
+
+
+
+console.log(audio1);
 });
